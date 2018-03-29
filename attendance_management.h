bb81a08e7a5747c9dfcc2dbb64b4cc7dba7373d8
Kavish <kavishgambhir21@gmail.com>
2018-03-29 20:33:50 +0530

    fixed header

--- attendance_management.h
+++ attendance_management.h
@@ -11,61 +11,94 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <map>
 using namespace std;
-class User
-{
-	string name;
+class Database_handler {
+    map<string, string> faculty;
 public:
-	void setName();
-	void getName();
-	void viewAttendance();
-	void viewDefaulters();
-};
-class Faculty : User
-{
-	string password;
-public:
-	void addStudent();
-	void deleteStudent();
-	void setStudent();
-	void modifyStudent();
-	void addAttendance();
-	void setPassword();
-	const void facultyDashboard();
-};
-class Student : User
-{
-	string rollNumber;
-};
-class Interface_handler
-{
-public:
-	void loginFaculty();
-	void _register();
-	void homeView();
-	void loginAdmin();
+    Database_handler();
+    void addStudent();
+
+    void getStudent();
+
+    void deleteStudent();
+
+    void addFaculty();
+
+    string getFaculty(string);
+
+    void deleteFaculty();
+
     friend bool passwordStrength(string);
 };
-class Admin : Faculty
-{
+
+class User {
+    string name;
 public:
-	void addFaculty();
-	void setFaculty();
-	void getFaculty();
-	void adminDashboard();
-	void deleteFaculty();
+    void setName();
+
+    void getName();
+
+    void viewAttendance();
+
+    void viewDefaulters();
+};
+
+class Faculty : public User {
+    string password;
+public:
+    void addStudent();
+
+    void deleteStudent();
+
+    void setStudent();
+
+    void modifyStudent();
+
+    void addAttendance();
+
+    void setPassword();
+
+    const void facultyDashboard();
+};
+
+class Student : public User {
+    string rollNumber;
+};
+
+class Interface_handler {
+    static int adminPasswordCount;
+    Database_handler *db;
+public:
+    Interface_handler();
+
+    void loginFaculty();
+
+    void _register();
+
+    void homeView();
+
+    void loginAdmin();
+
+    friend bool passwordStrength(string);
+};
+
+class Admin : public Faculty {
+private:
+    Interface_handler handler;
+public:
+    void addFaculty();
+
+    void setFaculty();
+
+    void getFaculty();
+
+    void adminDashboard();
+
+    void deleteFaculty();
 
 };
-class Database_handler
-{
-public:
-	static int callback(void *NotUsed, int argc, char **argv, char **azColName);
-	void addStudent();
-	void getStudent();
-	void deleteStudent();
-	void addFaculty();
-	void getFaculty();
-	void deleteFaculty();
-};
+
 
 #endif //ATTENDANCE_ATTENDANCE_MANAGEMENT_H
